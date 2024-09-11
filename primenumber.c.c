d27a3f5300f4684be50c1da4a99e50eeaa4e6269
rakshunataraj <rakshunataraj6@gmail.com>
2024-09-11 10:02:03 +0530

    Add files via upload

---
+++ primenumber.c.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main() {
+  int i, flag = 0, n;
+  printf("Enter the number: ");
+  scanf("%d", &n);
+
+  if(n<2){
+    printf("Not a prime number.\n");
+  return 0;
+  }
+  for(i = 2; i < n; i++) {
+    if (n % i == 0) {
+      flag = 1;
+      break; 
+    }
+  }
+  if (flag == 0) {
+    printf("Not a prime number.\n");
+  }
+  return 0;
+}
